17b765618a1a5c20ff923dfdae2affebd521072a
Daniil0131 <Daniil01413@yandex.ru>
2025-10-29 21:30:17 +0300

    Проверка дизайна-4

--- volpart/vite.config.js
+++ volpart/vite.config.js
@@ -1,9 +1,4 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-
 export default defineConfig({
-  // ЕСЛИ репозиторий называется "volpart" → '/volpart/'
-  // ЕСЛИ репозиторий называется "volpart.ru" → '/volpart.ru/'
-  base: '/volpart/',
-  build: { outDir: 'docs' }   // собираем туда, откуда Pages умеет отдавать
+  base: '/volpart/'  // имя репозитория
 })
