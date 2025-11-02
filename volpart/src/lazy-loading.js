57b95886f52154d2ef9005aa2cc965ac6c929c66
Daniil0131 <Daniil01413@yandex.ru>
2025-11-02 14:11:34 +0300

    update

---
+++ volpart/src/lazy-loading.js
@@ -0,0 +1,27 @@
+export function lazy_loading_down() {
+    return new IntersectionObserver((entries, obs) => {
+        entries.forEach(item => {
+            if(item.isIntersecting) {
+                item.target.classList.add("lazy_animation-down");
+                obs.unobserve(item.target);
+            }
+        })
+    }, {
+        threshold: 0.2
+    });
+    
+}
+export function lazy_loading_up() {
+    return new IntersectionObserver((entries, obs) => {
+        entries.forEach(item => {
+            if(item.isIntersecting) {
+                item.target.classList.remove("for_lazy-loading-off")
+                item.target.classList.add("for_lazy-loading-on")
+                item.target.classList.add("lazy_animation-up");
+                obs.unobserve(item.target);
+            }
+        })
+    }, {
+        threshold: 0.2
+    });
+}
--- volpart/src/main.js
+++ volpart/src/main.js
@@ -1,6 +1,30 @@
 import './styles/global.css'
 import './styles/style.css'
 import './styles/root.css'
+import './styles/animated.css'
 import './image/background_main.jpg'
+import { lazy_loading_down } from './lazy-loading'
+import { lazy_loading_up } from './lazy-loading'
+
+document.addEventListener('DOMContentLoaded', () => {
+const logo = document.querySelector('.header_logo-image');
+const header_title = document.querySelectorAll('.about h2, .about p');
+const apartment_title = document.querySelector('.apartment_title')
+const apartment_list = document.querySelectorAll('.apartment_list article')
+const apartment = document.querySelectorAll('.apartment')
+
+lazy_loading_down().observe(logo);
+const tar = lazy_loading_up()
+
+header_title.forEach(element => {
+    tar.observe(element)
+});
+
+lazy_loading_up().observe(apartment_title);
 
 
+apartment_list.forEach(element => {
+    tar.observe(element)
+});
+
+});
